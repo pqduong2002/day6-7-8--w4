9fb8a4f769213c851b2897d81ac4bdc2cb6120d4
pqduong2002 <duongpham1882002@gmail.com>
2024-01-13 13:23:41 +0700

    duong

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.image-slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      Infinity: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+
